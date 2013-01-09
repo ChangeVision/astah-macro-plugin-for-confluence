bbb5a301915c960835e4687de028a561bd2c731f
Hiroki Kondo <kompiro@gmail.com>
2013-01-09 18:38:51 +0900

    fixed to show diagrams when it seted Confluence's context path in CentOS

--- src/main/resources/js/previewImage.js
+++ src/main/resources/js/previewImage.js
@@ -1,13 +1,15 @@
-AJS.toInit(function() {
-    init_viewer(theId,theAttachmentId,theAttachmentVersion);
-    jQuery("#" + theId + "_diagram_image").colorbox({
-        href:function(){
-            return jQuery(this).attr("src");
-          },
-          title:function(){
-            return jQuery("#" + theId + "_diagramName").text();
-          },
-          scalePhotos:false,
-          scrolling:true
-          });
-});
+function previewImage( contextPath, theId, theAttachmentId, theAttachmentVersion){
+  AJS.toInit(function() {
+      init_viewer(contextPath, theId,theAttachmentId,theAttachmentVersion);
+      jQuery("#" + theId + "_diagram_image").colorbox({
+          href:function(){
+              return jQuery(this).attr("src");
+            },
+            title:function(){
+              return jQuery("#" + theId + "_diagramName").text();
+            },
+            scalePhotos:false,
+            scrolling:true
+            });
+  });
+}
